5b001cc80deabee18d0a02a9bf18334cb191ff71
OrangeBeatle123 <jamesammulligan@gmail.com>
2022-01-29 22:51:57 +0000

    Added featured projects to home page

--- components/Container.js
+++ components/Container.js
@@ -8,18 +8,24 @@ import {
 import NextLink from 'next/link'
 import styled from '@emotion/styled'
 import DarkModeSwitch from '../components/DarkModeSwitch'
+import { motion } from "framer-motion"
 
 const Container = ({ children }) => {
     const { colorMode } = useColorMode();
 
     const bgColor = {
         light: 'white',
-        dark: '#171717'
+        dark: '#171923'
     }
 
     const navHoverBg = {
         light: 'gray.600',
         dark: 'gray.300',
+    }
+
+    const color = {
+        light: 'black',
+        dark: 'white'
     }
     
     const StickNav = styled(Flex)`
@@ -32,23 +38,52 @@ const Container = ({ children }) => {
     
     return (
         <>
+            <motion.div
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: .7 }}
+            >
             <StickNav
-                
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+                maxWidth="800px"
+                minWidth="356px"
+                width="100%"
+                bg={bgColor[colorMode]}
+                as="nav"
+                px={[2, 4, 6]}
+                py={2}
+                mt={8}
+                mb={[0, 0, 8]}
+                mx="auto"
             >
                 <Box>
                     <NextLink href="/" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>                            Home
+                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
                             Home
                         </Button>
                     </NextLink>
                     <NextLink href="/" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>                            Home
+                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
                             Blog
                         </Button>
                     </NextLink>
                 </Box>
                 <DarkModeSwitch />
             </StickNav>
+            <Flex
+                as="main"
+                justifyContent="center"
+                flexDirection="column"
+                bg={bgColor[colorMode]}
+                color={color[colorMode]}
+                px={[0, 4, 4]}
+                mt={[4, 8, 8]}
+            >
+                {children}
+            </Flex>
+            </motion.div>
         </>
     )
 }
